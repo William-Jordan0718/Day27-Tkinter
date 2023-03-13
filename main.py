e1029092d4a17ad9d4aa1f40906c0d3f10948c7b
williamjordan <will.jordan0788@gmail.com>
2023-03-13 13:34:17 -0700

    added padding

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
-# import tkinter
-#
-# window = tkinter.Tk()
-# window.title("My First GUI Program")
-# window.minsize(width=500, height=300)
-#
-# #Label
-# my_label = tkinter.Label(text="I Am a label", font=("Arial", 24, "bold"))
-# my_label.pack()
-#
-#
-#
-#
-# window.mainloop()
+from tkinter import *
 
-# def add(*args):
-#     total = 0
-#     for n in args:
-#         total += n
-#     return total
-#
-#
-# print(add(3, 5, 6))
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
-# def bar(spam, eggs, toast='yes please!', ham=0):
-#     print(spam, eggs, toast, ham)
-#
-#
-# print(bar(1, 2))
+#Label
+my_label = Label(text="I Am a label", font=("Arial", 24, "bold"))
+my_label.grid()
+
+my_label["text"] = "New Text"
+# my_label.config(text="New Text")
 
 
-# def bar(spam, eggs, toast='yes please!', ham=0):
-#     print(spam, eggs, toast, ham)
-#
-#
-# bar(toast='nah', spam=4, eggs=2)
+# Button
 
-# def test(*args):
-#     print(args)
-#
-#
-# print(test(1, 2, 3, 5))
-
-def all_aboard(a, *args, **kw):
-    print(a, args, kw)
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 
-print(all_aboard(4, 7, 3, 0, x=10, y=64))
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+# Entry
+
+input = Entry()
+input.grid()
+
+window.mainloop()
+
+
 
 
 
