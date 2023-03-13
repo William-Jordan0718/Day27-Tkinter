474ecc692a0a7db00bbe1d16d54a3aabe0be2497
williamjordan <will.jordan0788@gmail.com>
2023-03-12 18:27:46 -0700

    'Added additional practice to understand args kwargs'

--- main.py
+++ main.py
@@ -13,16 +13,39 @@
 #
 # window.mainloop()
 
-def add(*args):
-    total = 0
-    for n in args:
-        total += n
-    return total
+# def add(*args):
+#     total = 0
+#     for n in args:
+#         total += n
+#     return total
+#
+#
+# print(add(3, 5, 6))
+
+# def bar(spam, eggs, toast='yes please!', ham=0):
+#     print(spam, eggs, toast, ham)
+#
+#
+# print(bar(1, 2))
 
 
-print(add(3, 5, 6))
+# def bar(spam, eggs, toast='yes please!', ham=0):
+#     print(spam, eggs, toast, ham)
+#
+#
+# bar(toast='nah', spam=4, eggs=2)
+
+# def test(*args):
+#     print(args)
+#
+#
+# print(test(1, 2, 3, 5))
+
+def all_aboard(a, *args, **kw):
+    print(a, args, kw)
 
 
+print(all_aboard(4, 7, 3, 0, x=10, y=64))
 
 
 
